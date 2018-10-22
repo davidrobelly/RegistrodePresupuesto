bd557a73aaf28cc6ea61282491a335a3ccd25ed4
David Robelly <david.robelly@gmail.com>
2018-10-22 16:13:41 -0500

    cambios

--- query base de presupuestos.sql
+++ query base de presupuestos.sql
@@ -72,7 +72,7 @@ total17 decimal (10,2)
 );
 
 INSERT INTO CentroCosto (idusuario, ccosto, nombre)
-values (2, 20078, 'SEMINARIOS DE GRADUACI”N PROTAL');
+values (2, 20078, 'SEMINARIOS DE GRADUACI√ìN PROTAL');
 INSERT INTO CentroCosto (idusuario, ccosto, nombre)
 values (2, 20079, 'SEMINARIOS VARIOS');
 
@@ -85,29 +85,29 @@ values (3, 26091, 'CAPACITACION EMPRESARIAL POSTGRADOS FEN');
 INSERT INTO CentroCosto (idusuario, ccosto, nombre)
 values (3, 26092, 'UTILIDADES FEN');
 INSERT INTO CentroCosto (idusuario, ccosto, nombre)
-values (3, 26104, 'FEN SEMINARIO DE GRADUACI”N');
+values (3, 26104, 'FEN SEMINARIO DE GRADUACI√ìN');
 INSERT INTO CentroCosto (idusuario, ccosto, nombre)
 values (3, 26168, 'FEN CURSOS VACACIONALES 2012');
 INSERT INTO CentroCosto (idusuario, ccosto, nombre)
-values (3, 26323, 'MAESTRIA EN ENSE—ANZA DE INGL…S');
+values (3, 26323, 'MAESTRIA EN ENSE√ëANZA DE INGL√âS');
 INSERT INTO CentroCosto (idusuario, ccosto, nombre)
-values (3, 26353, 'MAESTRIA EN POLÕTICAS Y GESTI”N P⁄BLICA');
+values (3, 26353, 'MAESTRIA EN POL√çTICAS Y GESTI√ìN P√öBLICA');
 INSERT INTO CentroCosto (idusuario, ccosto, nombre)
-values (3, 26365, 'MAESTRIA DE INVESTIGACI”N EN CIENCIAS ECON”MICAS');
+values (3, 26365, 'MAESTRIA DE INVESTIGACI√ìN EN CIENCIAS ECON√ìMICAS');
 INSERT INTO CentroCosto (idusuario, ccosto, nombre)
 values (3, 26367, 'CURSOS DE INGLES PARTICULARES Y TOEFL');
 INSERT INTO CentroCosto (idusuario, ccosto, nombre)
-values (3, 26446, 'INVESTIGACI”N LABORATORIO EXPERIMENTAL');
+values (3, 26446, 'INVESTIGACI√ìN LABORATORIO EXPERIMENTAL');
 INSERT INTO CentroCosto (idusuario, ccosto, nombre)
-values (3, 26453, 'PROGRAMA CAPACITACI”N BDE');
+values (3, 26453, 'PROGRAMA CAPACITACI√ìN BDE');
 INSERT INTO CentroCosto (idusuario, ccosto, nombre)
 values (3, 26462, 'CONGRESO LACEA LAMES');
 INSERT INTO CentroCosto (idusuario, ccosto, nombre)
-values (3, 26006, 'MAESTRIA EN ECONOMIA Y DIRECCI”N DE EMPRESAS');
+values (3, 26006, 'MAESTRIA EN ECONOMIA Y DIRECCI√ìN DE EMPRESAS');
 INSERT INTO CentroCosto (idusuario, ccosto, nombre)
 values (3, 26007, 'MAESTRIA  EN FINANZAS');
 INSERT INTO CentroCosto (idusuario, ccosto, nombre)
-values (3, 26008, 'MAESTRIA EN GESTI”N DEL TALENTO HUMANO');
+values (3, 26008, 'MAESTRIA EN GESTI√ìN DEL TALENTO HUMANO');
 
 
 
@@ -117,210 +117,7 @@ INSERT INTO Usuarios (usuarios, claves)
 values ('mreyes', '23mr78');
 INSERT INTO Usuarios (usuarios, claves)
 values ('lfsanche',	'26lfs06');
-INSERT INTO Usuarios (usuarios, claves)
-values ('amorlas',	'25ams80');
-INSERT INTO Usuarios (usuarios, claves)
-values ('roperugachi',	'14le53');
-INSERT INTO Usuarios (usuarios, claves)
-values ('direccionec',	'edu94ec');
-INSERT INTO Usuarios (usuarios, claves)
-values ('glbajanaj'	,'20pro77');
-INSERT INTO Usuarios (usuarios, claves)
-values ('pcornejo'	,'pi29cor');
-INSERT INTO Usuarios (usuarios, claves)
-values ('ldominguez'	,'10ld27');
-INSERT INTO Usuarios (usuarios, claves)
-values ('oaguilar'	,'10AR01');
-INSERT INTO Usuarios (usuarios, claves)
-values ('fecheverria'	,'10FE05');
-INSERT INTO Usuarios (usuarios, claves)
-values ('malchiluiza'	,'kma11ch');
-INSERT INTO Usuarios (usuarios, claves)
-values ('bramos'	,'11BR16');
-INSERT INTO Usuarios (usuarios, claves)
-values ('kastudillo'	,'11KA20');
-INSERT INTO Usuarios (usuarios, claves)
-values ('smatamoros'	,'11SM21');
-INSERT INTO Usuarios (usuarios, claves)
-values ('sflores'	,'11SF27');
-INSERT INTO Usuarios (usuarios, claves)
-values ('kberru'	,'14kbr149');
-INSERT INTO Usuarios (usuarios, claves)
-values ('javaldiviezo'	,'15jva44');
-INSERT INTO Usuarios (usuarios, claves)
-values ('jvaldiviezo'	,'15JV45');
-INSERT INTO Usuarios (usuarios, claves)
-values ('mmendoza'	,'mm16za');
-INSERT INTO Usuarios (usuarios, claves)
-values ('ogonzalez'	,'16OG74');
-INSERT INTO Usuarios (usuarios, claves)
-values ('hsanchez'	,'18HS39');
-INSERT INTO Usuarios (usuarios, claves)
-values ('ftorres'	,'21FT58');
-INSERT INTO Usuarios (usuarios, claves)
-values ('daysosa'	,'22sos60');
-INSERT INTO Usuarios (usuarios, claves)
-values ('epelaez'	,'23EP60');
-INSERT INTO Usuarios (usuarios, claves)
-values ('pherrera'	,'29PH05');
-INSERT INTO Usuarios (usuarios, claves)
-values ('paherrera'	,'32PH17');
-INSERT INTO Usuarios (usuarios, claves)
-values ('marbam'	,'36mbm36');
-INSERT INTO Usuarios (usuarios, claves)
-values ('mpisco'	,'39MP75');
-INSERT INTO Usuarios (usuarios, claves)
-values ('lzhingri'	,'40LZ76');
-INSERT INTO Usuarios (usuarios, claves)
-values ('pcarrion'	,'42PCA82');
-INSERT INTO Usuarios (usuarios, claves)
-values ('bvintimilla'	,'43BV28');
-INSERT INTO Usuarios (usuarios, claves)
-values ('palmeida'	,'16PA72');
-INSERT INTO Usuarios (usuarios, claves)
-values ('douplaza'	,'11dou18');
-INSERT INTO Usuarios (usuarios, claves)
-values ('INUNEZ'	,'15INU42');
-INSERT INTO Usuarios (usuarios, claves)
-values ('IRIVAD'	,'26IRI61');
-INSERT INTO Usuarios (usuarios, claves)
-values ('ssonnen'	,'10SS54');
-INSERT INTO Usuarios (usuarios, claves)
-values ('kmoran'	,'10km34');
-INSERT INTO Usuarios (usuarios, claves)
-values ('mmedina'	,'30mm90');
-INSERT INTO Usuarios (usuarios, claves)
-values ('postfcnm'	,'22post72');
-INSERT INTO Usuarios (usuarios, claves)
-values ('emontero'	,'18EM259');
-INSERT INTO Usuarios (usuarios, claves)
-values ('aespinal'	,'11AES64');
-INSERT INTO Usuarios (usuarios, claves)
-values ('cmonsalve'	,'11CM292');
-INSERT INTO Usuarios (usuarios, claves)
-values ('fvera'	,'15fv39');
-INSERT INTO Usuarios (usuarios, claves)
-values ('wcarvache'	,'13wc05');
-INSERT INTO Usuarios (usuarios, claves)
-values ('ctobalina'	,'10ct247');
-INSERT INTO Usuarios (usuarios, claves)
-values ('vasalazar'	,'15vs45');
-INSERT INTO Usuarios (usuarios, claves)
-values ('vlasio'	,'32VL25');
-INSERT INTO Usuarios (usuarios, claves)
-values ('joberm'	,'10vg31');
-INSERT INTO Usuarios (usuarios, claves)
-values ('cmerag'	,'11cm32');
-INSERT INTO Usuarios (usuarios, claves)
-values ('cornejo'	,'10ma48');
-INSERT INTO Usuarios (usuarios, claves)
-values ('daochoa'	,'29do336');
-INSERT INTO Usuarios (usuarios, claves)
-values ('angeram'	,'14anr03');
-INSERT INTO Usuarios (usuarios, claves)
-values ('xaordeÒana'	,'32xor34');
-INSERT INTO Usuarios (usuarios, claves)
-values ('ropalacios'	,'32ropa35');
-INSERT INTO Usuarios (usuarios, claves)
-values ('emoncayo'	,'46emo39');
-INSERT INTO Usuarios (usuarios, claves)
-values ('jamaldonado'	,'38jama41');
-INSERT INTO Usuarios (usuarios, claves)
-values ('cinveintimi'	,'13cin38');
-INSERT INTO Usuarios (usuarios, claves)
-values ('kbarcia'	,'10kb09');
-INSERT INTO Usuarios (usuarios, claves)
-values ('bbayot'	,'44bba45');
-INSERT INTO Usuarios (usuarios, claves)
-values ('acevallos'	,'32ac47');
-INSERT INTO Usuarios (usuarios, claves)
-values ('jsantos'	,'13JS37');
-INSERT INTO Usuarios (usuarios, claves)
-values ('jumacev'	,'47juce68');
-INSERT INTO Usuarios (usuarios, claves)
-values ('JERODLE'	,'JEN44RO');
-INSERT INTO Usuarios (usuarios, claves)
-values ('plromero'	,'plr12030');
-INSERT INTO Usuarios (usuarios, claves)
-values ('lucisnero'	,'16lu071');
-INSERT INTO Usuarios (usuarios, claves)
-values ('jlayana'	,'11jol79');
-INSERT INTO Usuarios (usuarios, claves)
-values ('jgavilanes'	,'13jug38');
-INSERT INTO Usuarios (usuarios, claves)
-values ('cricaurt'	,'47crq62');
-INSERT INTO Usuarios (usuarios, claves)
-values ('gcaicedo'	,'33guc86');
-INSERT INTO Usuarios (usuarios, claves)
-values ('mcarrasco'	,'10moc91');
-INSERT INTO Usuarios (usuarios, claves)
-values ('aherrera'	,'14ph68');
-INSERT INTO Usuarios (usuarios, claves)
-values ('herrera'	,'49PH11');
-INSERT INTO Usuarios (usuarios, claves)
-values ('wcardem'	,'49wm90');
-INSERT INTO Usuarios (usuarios, claves)
-values ('jgmarc'	,'25jma80');
-INSERT INTO Usuarios (usuarios, claves)
-values ('jgcastillo'	,'26jgc04');
-INSERT INTO Usuarios (usuarios, claves)
-values ('dnoboa'	,'do103no');
-INSERT INTO Usuarios (usuarios, claves)
-values ('cecorral'	,'cec095or');
-INSERT INTO Usuarios (usuarios, claves)
-values ('pacalle'	,'49pca23');
-INSERT INTO Usuarios (usuarios, claves)
-values ('paulher'	,'12her42');
-INSERT INTO Usuarios (usuarios, claves)
-values ('glarinc'	,'47glrp21');
-INSERT INTO Usuarios (usuarios, claves)
-values ('raespine'	,'ra49spin');
-INSERT INTO Usuarios (usuarios, claves)
-values ('jchang'	,'21JC72');
-INSERT INTO Usuarios (usuarios, claves)
-values ('gbajanaj'	,'49gb19');
-INSERT INTO Usuarios (usuarios, claves)
-values ('respinel'	,'rae42sp');
-INSERT INTO Usuarios (usuarios, claves)
-values ('gusberm'	,'11gus19');
-INSERT INTO Usuarios (usuarios, claves)
-values ('cabadr'	,'cabad143');
-INSERT INTO Usuarios (usuarios, claves)
-values ('pauher'	,'49pah43');
-INSERT INTO Usuarios (usuarios, claves)
-values ('anyane'	,'13yaz44');
-INSERT INTO Usuarios (usuarios, claves)
-values ('edchav'	,'49edc47');
-INSERT INTO Usuarios (usuarios, claves)
-values ('cesyep'	,'11cey52');
-INSERT INTO Usuarios (usuarios, claves)
-values ('edjimen'	,'49edj00');
-INSERT INTO Usuarios (usuarios, claves)
-values ('edcervant'	,'40edc18');
-INSERT INTO Usuarios (usuarios, claves)
-values ('camonsal'	,'47cmo64');
-INSERT INTO Usuarios (usuarios, claves)
-values ('adsantos'	,'47asan21');
-INSERT INTO Usuarios (usuarios, claves)
-values ('daorte'	,'d514ort');
-INSERT INTO Usuarios (usuarios, claves)
-values ('ta40bo'	,'10tb61');
-INSERT INTO Usuarios (usuarios, claves)
-values ('kadaleta'	,'26kle07');
-INSERT INTO Usuarios (usuarios, claves)
-values ('fvalare'	,'fr29val');
-INSERT INTO Usuarios (usuarios, claves)
-values ('mvillavi'	,'47mvi64');
-INSERT INTO Usuarios (usuarios, claves)
-values ('financieroec'	,'fin94edu');
-INSERT INTO Usuarios (usuarios, claves)
-values ('yborja'	,'26ybo62');
-INSERT INTO Usuarios (usuarios, claves)
-values ('lposligu'	,'lpos110');
-INSERT INTO Usuarios (usuarios, claves)
-values ('aridobra'	,'aribra28');
-INSERT INTO Usuarios (usuarios, claves)
-values ('meromero'	,'26mero62');
+
 
 
 
